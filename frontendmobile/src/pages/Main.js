f65fcd4760f1b7126c17b3db5c0c326000817bf1
Diego Montalvao <caals19@gmail.com>
2020-01-16 19:33:01 -0400

    Fazendo o textIpunt fazer paddind no acionar o teclado virtual

--- frontendmobile/src/pages/Main.js
+++ frontendmobile/src/pages/Main.js
@@ -1,9 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
+
+const [isKeyboardVisible, setisKeyboardVisible] = useState(false);
+
+ useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+        'keyboardDidShow', 
+        () => {
+            setisKeyboardVisible(true);
+        }
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setisKeyboardVisible(false);
+      }
+    );
+    
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
     const [currentRegion, setcurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -14,8 +39,6 @@ function Main({ navigation }) {
                 const { coords } = await getCurrentPositionAsync({
                     enableHighAccuracy: true,                    
                 });
-
-                // console.log(coords);
 
                 const { latitude, longitude } = coords;
 
@@ -36,27 +59,44 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -16.4593664, longitude: -54.6422784 }}>
-                <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/59020856?v=4" }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'DiegoMontalvao' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Diego Montalvão</Text>
-                        <Text style={styles.devBio}>Rockie in this beautiful world of web development</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <View style={styles.container}>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -16.4593664, longitude: -54.6422784 }}>
+                    <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/59020856?v=4" }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'DiegoMontalvao' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Diego Montalvão</Text>
+                            <Text style={styles.devBio}>Rockie in this beautiful world of web development</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                style={isKeyboardVisible === false ? styles.searchInput : styles.searchInputTrue}
+                placeholder="Buscar devs por techs..."
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" syze={20} color="#fff" />
+                </TouchableOpacity>   
+            </View>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    map: {
+    container: {
         flex: 1
     },
+    map: {
+        flex: 1
+    },    
     avatar: {
         width: 54,
         height: 54,
@@ -75,10 +115,60 @@ const styles = StyleSheet.create({
         color: '#666',
         marginTop: 5,
     },
-    Techs: {
+    devTechs: {
         marginTop: 5,
     },
-
+    searchForm:  {
+        position: 'absolute',
+        bottom: 10,     
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+        marginBottom: 0,
+    },
+    searchInputTrue: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+        marginBottom: 300,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
 })
 
 export default Main;
